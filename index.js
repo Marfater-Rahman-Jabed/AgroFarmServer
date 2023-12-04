825ea0f804529153a6c8740b050e816c05ce1598
Marfater Rahman Jabed <marfaterrahman@gmail.com>
2023-12-04 23:59:35 +0600

    added upload vegitable

--- index.js
+++ index.js
@@ -5,20 +5,12 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 
-//
-// 
-
-
-
 app.use(express.json())
 app.use(cors())
 
-
-// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.4jznvny.mongodb.net/?retryWrites=true&w=majority`;
 console.log((uri))
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -29,17 +21,29 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        const itemsCollection = client.db('AgroFarm').collection('itemsCollection')
+        const VegetableCollection = client.db('AgroFarm').collection('VegetableCollection')
 
 
-        app.get('/products/:id', async (req, res) => {
-            const id = req.params.id
-            const query = {
-                _id: new ObjectId(id)
-            };
-            const cursor = itemsCollection.find(query);
+        app.get('/products', async (req, res) => {
+            const query = {}
+            const cursor = VegetableCollection.find(query).sort({ _id: -1 });
             const result = await cursor.toArray()
             res.send(result)
+        })
+
+        app.post('/uploadVegetableProduct', async (req, res) => {
+            const query = req.body;
+            const result = await VegetableCollection.insertOne(query)
+            res.send(result)
+        })
+
+        app.delete('/deleteProducts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {
+                _id: new ObjectId(id)
+            }
+            const result = await VegetableCollection.deleteOne(query)
+            res.send(result);
         })
 
 
